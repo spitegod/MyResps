cf6dff86ce6929fa5ff5de92f7db0781a641db25
spitegod <iermakov2207@gmail.com>
2026-03-16 17:12:02 +0300

    refactor: split into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
 import { useState } from "react"
+import "./App.css"
+import { CreateTask } from "./components/CreateTask"
+import { Task } from "./components/Task"
+
+export type TaskItem = {
+  task: string
+}
 
 function App() {
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState<{ task: string }[]>([])
+  const [tasks, setTasks] = useState<TaskItem[]>([])
 
-  const onCreateBtnHandler = () => {
-    setTasks([...tasks, { task }])
-    setTask('')
+  const onCreateTask = (task: string) => {
+    const trimmedTask = task.trim()
+
+    if (!trimmedTask) {
+      return
+    }
+
+    setTasks((prevTasks) => [...prevTasks, { task: trimmedTask }])
   }
 
-  const onDeleteBtnHandler = (indexToDelete: number) => {
-    setTasks(tasks.filter((_, index) => index !== indexToDelete))
+  const onDeleteTask = (indexToDelete: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.filter((_, index) => index !== indexToDelete),
+    )
   }
-
 
   return (
     <>
       <h1>MyResps</h1>
       <p>Create your task</p>
-      <div>
-        <input id='input_task' placeholder="Task" value={task} onChange={(e) => setTask(e.target.value)}></input>
-        <button onClick={onCreateBtnHandler}>Create</button>
-      </div>
+      <CreateTask onCreateTask={onCreateTask} />
       <h2>Created Tasks</h2>
       {tasks.map((taskItem, index) => (
-        <div key={index}>
-          <p>{taskItem.task}</p>
-          <button onClick={() => onDeleteBtnHandler(index)}>Delete</button>
-        </div>
+        <Task
+          key={`${taskItem.task}-${index}`}
+          task={taskItem}
+          onDelete={() => onDeleteTask(index)}
+        />
       ))}
     </>
   )
